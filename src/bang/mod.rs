489d437b7f2b66a96c45a45d009d34effac24e2e
Paul  d'Hubert <paul@tymate.com>
2019-01-24 10:11:32 +0100

    feat: add `bang::Dispatcher::dispatch` method

--- src/bang/mod.rs
+++ src/bang/mod.rs
@@ -8,37 +8,45 @@ use rocket::response;
 use std::collections::HashMap;
 
 pub enum Result {
-    Redirect { location: String },
-    BadRequest
+        Redirect { location: String },
+        BadRequest,
 }
 
 impl<'a> response::Responder<'a> for Result {
-    fn respond_to(self, request: &rocket::Request) -> response::Result<'a> {
-        match self {
-            Result::Redirect { location } => {
-                response::Redirect::to(location).respond_to(request)
-            },
-            _ => response::status::BadRequest(Some("could not process query")).respond_to(request),
+        fn respond_to(self, request: &rocket::Request) -> response::Result<'a> {
+                match self {
+                        Result::Redirect { location } => {
+                                response::Redirect::to(location).respond_to(request)
+                        }
+                        _ => response::status::BadRequest(Some("could not process query"))
+                                .respond_to(request),
+                }
         }
-    }
 }
 
 pub trait Handler: Send + Sync {
-    fn handle(&self, query: query::Query) -> Result;
+        fn handle(&self, query: query::Query) -> Result;
 }
 
 pub struct Dispatcher<'a, 'b> {
-    pub bangs: HashMap<&'a str, Box<&'b Handler>>,
+        pub bangs: HashMap<&'a str, Box<&'b Handler>>,
 }
 
 impl<'a, 'b> Dispatcher<'a, 'b> {
-    pub fn new() -> Self {
-        Self{
-            bangs: HashMap::new(),
+        pub fn new() -> Self {
+                Self {
+                        bangs: HashMap::new(),
+                }
         }
-    }
 
-    pub fn add(&mut self, key: &'a str, handler: &'b Handler) {
-        self.bangs.entry(key).or_insert(Box::new(handler));
-    }
+        pub fn add(&mut self, key: &'a str, handler: &'b Handler) {
+                self.bangs.entry(key).or_insert(Box::new(handler));
+        }
+
+        pub fn dispatch(&self, query: Query) -> Result {
+                query.bang
+                        .and_then(|bang| self.bangs.get(bang))
+                        .map(|handler| handler.handle(query))
+                        .unwrap_or(Result::BadRequest)
+        }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,35 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate nom;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate nom;
 
 mod bang;
 
-struct GitlabProject {
-}
+struct GitlabProject {}
 
 impl bang::Handler for GitlabProject {
-    fn handle(&self, _query: bang::Query) -> bang::Result {
-        bang::Result::Redirect {
-            location: "https://example.com".to_string(),
+        fn handle(&self, _query: bang::Query) -> bang::Result {
+                bang::Result::Redirect {
+                        location: "https://example.com".to_string(),
+                }
         }
-    }
 }
 
 use rocket::State;
 
 #[get("/?<q>")]
-fn handle_bang_query(
-    q: String, dispatcher: State<bang::Dispatcher>
-) -> bang::Result {
-    let query = bang::Query::parse(&q);
-
-    // println!("query: {:?}", query);
-
-    if let Some(bang) = query.bang {
-        if let Some(handler) = dispatcher.bangs.get(bang) {
-            handler.handle(query)
-        } else {
-            bang::Result::BadRequest
-        }
-    } else {
-        bang::Result::BadRequest
-    }
+fn handle_bang_query(q: String, dispatcher: State<bang::Dispatcher>) -> bang::Result {
+        dispatcher.dispatch(bang::Query::parse(&q))
 }
 
 fn main() {
-    let mut dispatcher = bang::Dispatcher::new();
-    dispatcher.add("gl", &GitlabProject{});
+        let mut dispatcher = bang::Dispatcher::new();
+        dispatcher.add("gl", &GitlabProject {});
 
-    rocket::ignite()
-        .manage(dispatcher)
-        .mount("/", routes![handle_bang_query])
-        .launch();
+        rocket::ignite()
+                .manage(dispatcher)
+                .mount("/", routes![handle_bang_query])
+                .launch();
 }
